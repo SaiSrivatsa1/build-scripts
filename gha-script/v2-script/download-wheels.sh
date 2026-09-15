21c5ec3c9c28e3b5c4a68c966f05924ec4b23e3c
SaiSrivatsa <Srighakollapu.Srivatsa1@ibm.com>
2026-09-15 16:25:28 +0530

    update download-wheel

--- gha-script/v2-script/download-wheels.sh
+++ gha-script/v2-script/download-wheels.sh
@@ -12,8 +12,8 @@ COS_ENDPOINT="https://s3.us-east.cloud-object-storage.appdomain.cloud"
 BUCKET="powercore-wheels-dev"
 
 CONFIG_URL="${COS_ENDPOINT}/${BUCKET}/powercore-config.env"
-BUCKET_URL="${COS_ENDPOINT}/${BUCKET}"
-LIST_URL="${BUCKET_URL}?list-type=2&prefix=s390x/"
+BUCKET_URL="${COS_ENDPOINT}/${BUCKET}/s390x"
+LIST_URL="${COS_ENDPOINT}/${BUCKET}?list-type=2"
 
 echo "--- Download config ---"
 echo "  CONFIG_URL        : ${CONFIG_URL}"
